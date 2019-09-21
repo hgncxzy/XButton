264f268542aa36795e1308199f0a1d30e85a040c
hgncxzy <hgncxzy@qq.com>
2019-09-21 11:00:52 +0800

    first commit

---
+++ app/src/main/java/com/xzy/ui/xbutton/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xzy.ui.xbutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
